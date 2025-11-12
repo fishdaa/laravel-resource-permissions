38830c63c444b98ab777f605f46d525687188d46
fishdaa <git@fishda.net>
2025-11-12 10:48:28 +0800

    init

---
+++ src/LaravelResourcePermissionsServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Vendor\LaravelResourcePermissions;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelResourcePermissionsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
+
